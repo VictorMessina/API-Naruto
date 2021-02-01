cc7a6d01f921487155cff893255de3c5cca707ab
Victor Messina <victormessina95@hotmail.com>
2021-02-01 00:05:01 -0300

    Rewrite API paths

--- app/routes/apiRoutes.js
+++ app/routes/apiRoutes.js
@@ -2,12 +2,12 @@ module.exports = function(application) {
 
     /** -------------------------------------- Rotas de GET -------------------------------------- */
 
-	application.get('/characters/allCharacters', function(req, res) {
-		application.app.controllers.characters.charactersController.allCharacters(application, req, res);
+	application.get('/api/characters/allCharacters', function(req, res) {
+		application.app.controllers.api.apiController.allCharacters(application, req, res);
     });
 
-    application.get('/characters/findCharacterByName', function(req, res) {
-		application.app.controllers.characters.charactersController.findCharacterByName(application, req, res);
+    application.get('/api/characters/findCharacterByName', function(req, res) {
+		application.app.controllers.api.apiController.findCharacterByName(application, req, res);
 	});
 
 	/** -------------------------------------- Rotas de POST -------------------------------------- */
