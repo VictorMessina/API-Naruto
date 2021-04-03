03a97a7723989c489ad3cde9ea91da66d6832586
Victor Messina <victormessina95@hotmail.com>
2021-04-03 17:09:26 -0300

    Fix indentation and create new endpoint to add characters data from XLSX

--- app/routes/apiRoutes.js
+++ app/routes/apiRoutes.js
@@ -1,15 +1,18 @@
-module.exports = function(application) {
+module.exports = function (application) {
 
-    /** -------------------------------------- Rotas de GET -------------------------------------- */
+	/** -------------------------------------- Rotas de GET -------------------------------------- */
 
-	application.get('/api/characters/allCharacters', function(req, res) {
+	application.get('/api/characters/allCharacters', function (req, res) {
 		application.app.controllers.api.apiController.allCharacters(application, req, res);
-    });
+	});
 
-    application.get('/api/characters/findCharacterByName', function(req, res) {
+	application.get('/api/characters/findCharacterByName', function (req, res) {
 		application.app.controllers.api.apiController.findCharacterByName(application, req, res);
 	});
 
 	/** -------------------------------------- Rotas de POST -------------------------------------- */
 
+	application.post('/api/characters/addCharactersFromXLSX', function (req, res) {
+		application.app.controllers.api.apiController.addCharactersFromXLSX(application, req, res);
+	});
 };
