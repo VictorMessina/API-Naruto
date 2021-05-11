00caf8a506c96777b137f4ab7150e3d819ca9952
Victor Messina <victormessina95@hotmail.com>
2021-05-10 21:38:49 -0300

    create creature model

---
+++ app/models/creature.js
@@ -0,0 +1,39 @@
+var mongoose = require('../../config/mongoConnection')
+const schema = mongoose.Schema;
+
+const creatureSchema = new schema({
+	name: {
+		type: String,
+		require: true
+	},
+    villageOriginalName: {
+		type: String,
+		require: true
+	},
+	villageTranslatedName: {
+		type: String,
+		require: true
+	},
+    biju: {
+        type: String,
+        require: true
+    },
+    nickname: {
+        type: String,
+        require: true
+    },
+    imageURL: {
+        type: String,
+		require: true
+    }
+});
+
+const creatureModel = mongoose.model('creatures', creatureSchema)
+
+function Creature() {
+	this.creatureModel = creatureModel;
+}
+
+module.exports = function () {
+	return Creature;
+};
